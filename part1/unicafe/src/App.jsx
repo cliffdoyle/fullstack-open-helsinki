d76fa8f4bdc5096af74cd1b1fa36319d9b36ae45
cliffdoyle <oyoocliff471@gmail.com>
2025-07-05 00:25:10 +0300

    updates: Add unicafe step 2 implementation with addition of more statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,12 +7,19 @@ function App() {
    const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const all=good+bad+neutral
+    const average=(good*1 + neutral*0 + bad*-1)/all
+    const positiveFeedback=(good/all)*100
+
   return (
     <div>
       <h1>Statistics</h1>
       <h3>Good FeedBack: {good}</h3>
       <h3>Neutral FeedBack: {neutral}</h3>
       <h3>Bad FeedBack: {bad}</h3>
+      <h3>All: {all}</h3>
+      <h3>Average: {average}</h3>
+      <h3>Positive Feedback:{positiveFeedback}%</h3>
       <div>
       <h1>Give Feedback</h1>
         <button onClick={()=>setGood(good+1)}>Good Button</button>
