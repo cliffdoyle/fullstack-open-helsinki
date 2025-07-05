4e7b791e4ce20a1fe9df16d5a35afd3d99b80a75
cliffdoyle <oyoocliff471@gmail.com>
2025-07-05 14:12:15 +0300

    refactor: created Button and StatisticLine components Split feedback and statistic display into separate components for better modularity and readability

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,49 +2,83 @@ import { useState } from 'react'
 
 
 
-
 function Statistics({props}){
   const {good,neutral,bad,all,average,positiveFeedback}=props
   console.log('props from App',good)
 
-  return(
-    <div>
-      <h1>Statistics</h1>
-      <h3>Good FeedBack: {good}</h3>
-      <h3>Neutral FeedBack: {neutral}</h3>
-      <h3>Bad FeedBack: {bad}</h3>
-      <h3>All: {all}</h3>
-      <h3>Average: {average}</h3>
-      <h3>Positive Feedback:{positiveFeedback}%</h3>
-      </div>
+  if (good !=0 || neutral != 0 || bad !=0 || all !=0){
 
+    return(
+      <div>
+  
+        <h1>Statistics</h1>
+      <StatisticLine text="good" value ={good} />
+      <StatisticLine text="neutral" value ={neutral} />
+      <StatisticLine text="bad" value ={bad} />
+      <StatisticLine text="all" value ={all} />
+      <StatisticLine text="average" value ={average} />
+      <StatisticLine text="positiveFeedback" value ={positiveFeedback}/>
+        </div>
+  
+    )
+  }else{
+    return(
+      <div>
+        <h1>Statistics</h1>
+        <h3>Please give feedback first</h3>
+
+      </div>
+    )
+
+    
+  }
+
+
+}
+
+const StatisticLine=({text,value})=>{
+  console.log(text, value)
+  return (
+    <div>
+      <h3>{text} {value}</h3>
+
+
+    </div>
+  )
+
+}
+const Button=({stats:{good,bad,neutral}, setters:{setGood,setBad,setNeutral}})=>{
+
+  return (
+    <div>
+      <button onClick={()=>setGood(good+1)}>Good Button</button>
+      <button onClick={()=>setNeutral(neutral+1)}>Neutral Button</button>
+      <button onClick={()=>setBad(bad+1)}>Bad Button</button>
+
+    </div>
   )
 
 }
 
 
 
+
 function App() {
   //Save clicks of each button to its own state
   const [good, setGood] = useState(0)
-   const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    const all=good+bad+neutral
-    const average=(good*1 + neutral*0 + bad*-1)/all
-    const positiveFeedback=(good/all)*100
-
+  const all=good+bad+neutral
+  const average=(good*1 + neutral*0 + bad*-1)/all
+  const positiveFeedback=(good/all)*100 
   return (
     <div>
-      <Statistics props={{good,neutral,bad,all,average,positiveFeedback}}/>
       <div>
       <h1>Give Feedback</h1>
-        <button onClick={()=>setGood(good+1)}>Good Button</button>
-           <button onClick={()=>setNeutral(neutral+1)}>Neutral Button</button>
-                      <button onClick={()=>setBad(bad+1)}>Bad Button</button>
+      <Button stats={{good,neutral,bad}} setters={{setGood,setBad,setNeutral}}/>
+      <Statistics props={{good,neutral,bad,all,average,positiveFeedback}}/>
       </div>
-
-    
     </div>
   )
 }
