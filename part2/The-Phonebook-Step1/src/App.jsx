21fb1773602aa3b2a37875e9956f04394471a361
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-07-08 14:40:27 +0300

    Add The Phonebook Step 3 exercise Adding a new state that tracks numbers Add new event handler that adds numbers to the persons object

--- part2/The-Phonebook-Step1/src/App.jsx
+++ part2/The-Phonebook-Step1/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 
 function App() {
   const [persons, setPersons] = useState([
-    {name:'Arto Hellas',id:1}
+    {name:'Arto Hellas',id:1,number:+254703836264}
   ])
   const [newName, setNewName]=useState('')
+  const [newNumber,setNewNumber]=useState()
 
   
   
@@ -14,7 +15,8 @@ function App() {
 
     const newNamey={
       name:newName,
-      id: persons.length+1
+      id: persons.length+1,
+      number:newNumber
     }
     // for (let i=0;i<persons.length;i++){
     //   // if (persons[i].name==newNamey.name){
@@ -32,10 +34,16 @@ function App() {
     }
     setPersons(persons.concat(newNamey))
     setNewName('')
+    setNewNumber('')
+
   }
   const addName=(event)=>{
 
     setNewName(event.target.value)
+  }
+
+  const addNumber=(event)=>{
+    setNewNumber(event.target.value)
   }
 
   console.log('new name:',newName)
@@ -49,14 +57,21 @@ function App() {
         <div>
           name: <input value={newName} onChange={addName}/>
         </div>
+        <div>
+          number:<input value={newNumber} onChange={addNumber}/>
+        </div>
 
         <div>
-          <button type='submit'>add</button>
+          <button type='submit'>AddDetails</button>
         </div>
         </form>
         <h2>Numbers</h2>
         <ul>
-        {persons.map((val)=><div key={val.id}><li>{val.name}</li></div>)}
+        {persons.map((val)=>
+        <div key={val.id}>
+          <li>{val.name} {val.number}</li>
+
+          </div>)}
         </ul>
      
     </div>
