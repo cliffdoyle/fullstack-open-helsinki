f220372f9ae9a3cab7c35e4eb8622897ec97b8fb
cliffdoyle <oyoocliff471@gmail.com>
2025-07-12 16:57:33 +0300

    update

---
+++ main.go
@@ -0,0 +1,63 @@
+// //Higher order functions, functions that take other functions as higher order functions
+
+// //Filter, map and reduce are functions on the array object
+// 	const animal=[
+// 		{Name: "Tom",Species: "Cat"},
+// 		{Name: "Jerry", Species: "Mouse"},
+// 		{Name: "pluto", Species: "Dog"},
+// 		{Name: "kelly",Species: "Dog"},
+// 	]
+
+// 	const orders= [
+// 		{amount:250},
+// 		{amount:340},
+// 		{amount:700},
+// 	]
+
+// 	const obj=[1,2,3,4,4,2,8,9]
+// 	const sum=orders.reduce((sum,acc)=>{
+// 		console.log("values :",sum,acc)
+// 		return sum+=acc.amount
+// 	},0)
+// 	console.log('Sum by reduce:',sum)
+
+// 	// const Dogs=obj.reject((val)=>val==4)
+// 	// console.log(Dogs);
+
+// 	// const names=[]
+
+// 	// for (let i=0;i<animal.length;i++)
+// 	// {
+// 	// 	names.push(animal[i].Name)
+// 	// }
+
+// 	//Map and filters are funct
+
+// 	const names=animal.map((val)=>(
+// 		// console.log(val)
+// 		val.Species+"l"
+
+// 	))
+
+// 	console.log(names);
+
+// 	//Map transforms the elements of the array
+
+package main 
+
+import "fmt"
+
+
+
+func main(){
+	arr1:=[]int{1,3,8,9,6}
+	arr2:=[]int{}
+
+	arr2=append(arr1[0:2],arr1[3:]...)
+
+	fmt.Println("array2",arr2)
+	fmt.Println("array1",arr1)
+}
+
+
+
